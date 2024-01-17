bc503f4b7883781bfc89c077c0a926304108abe5
omargnagyy <omargnagy@hotmail.com>
2024-01-17 05:04:17 +0200

    .

--- src/widget-registration.php
+++ src/widget-registration.php
@@ -4,14 +4,11 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-/**
- * Register the LinkedIn Posts Slider widget
- */
-function register_linkedin_posts_slider_widget()
+function register_slider_widget($widgets_manager)
 {
-    if (did_action('elementor/loaded')) {
-        require_once plugin_dir_path(__FILE__) . 'slider-widget.php';
-        \Elementor\Plugin::instance()->widgets_manager->register_widget_type(new \Elementor_Linkedin_Posts_Slider_Widget());
-    }
+    //require_once(__DIR__ . '/widgets/slider-widget.php');
+    require_once plugin_dir_path(__FILE__) . 'slider-widget.php';
+    $widgets_manager->register(new \Elementor_Linkedin_Posts_Slider_Widget());
 }
-add_action('elementor/widgets/widgets_registered', 'register_linkedin_posts_slider_widget');
+
+add_action('elementor/widgets/register', 'register_slider_widget');
