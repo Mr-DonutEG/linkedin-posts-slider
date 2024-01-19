c41dad7084330517fc08e82863f6973b8bfc1fb3
omargnagyy <omargnagy@hotmail.com>
2024-01-19 08:22:26 +0200

    .

--- src/slider-widget.php
+++ src/slider-widget.php
@@ -10,6 +10,24 @@ if (!defined('ABSPATH')) {
 
 
 /**
+ * Register scripts and styles for Elementor test widgets.
+ */
+function elementor_lps_widget_dependencies()
+{
+
+  wp_register_style('swiper-style', plugins_url('../public/swiperjs/swiper-bundle.css', __FILE__));
+  wp_register_script('swiper-script', plugins_url('../public/swiperjs/swiper-bundle.js', __FILE__), ['jquery'], false, true);
+
+  wp_register_style('linkedin-slider-style', plugins_url('../public/styles.css', __FILE__));
+  // Localize the script with the AJAX URL
+  wp_localize_script('swiper-script', 'ajax_object', array(
+    'ajax_url' => admin_url('admin-ajax.php'),
+    // You can add more data here if needed
+  ));
+}
+add_action('wp_enqueue_scripts', 'elementor_lps_widget_dependencies');
+
+/**
  * Elementor LinkedIn Posts Slider Widget.
  */
 class Elementor_Linkedin_Posts_Slider_Widget extends \Elementor\Widget_Base
